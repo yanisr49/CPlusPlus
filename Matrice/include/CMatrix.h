5f039a67a4ba4d759e5f7ab985efa1a8d276d1f7
RomainHerault <heraultromain8@gmail.com>
2018-04-06 10:21:08 +0200

    Tupu

--- Matrice/include/CMatrix.h
+++ Matrice/include/CMatrix.h
@@ -6,28 +6,29 @@
 #include <list>
 #include <iostream>
 #include <assert.h>
+#include "CException.h"
+
 
 template <class NType>
 class CMatrix
 {
 private:
-	NType ppMATMatrix[][];
 
 	unsigned int uiMATRow;
 
-	unsigned int uiMatCol;
+	unsigned int uiMATCol;
 
+    NType **ppMATMatrix;
 
 public:
-	void CMatrix(int iRow, int iCol, NType ppTab[][]);
+	 CMatrix(unsigned int uiRow, unsigned int uiCol, NType **ppTab);
 
-	void CMatrix(CMatrix MATParam);
+	 CMatrix(const CMatrix & MATParam);
 
-	void CMatrix(int iRow, int iCol);
+	 CMatrix(unsigned int uiRow, unsigned int uiCol);
 
-	CMatrix operator+(CMatrix MATParam, double c);
+	CMatrix& operator*(double c);
 
-	CMatrix operator-(CMatrix MATParam, double c);
 
 	CMatrix operator/(double c);
 
@@ -42,4 +43,155 @@ public:
 	CMatrix MATTranspose();
 
 };
+
+//Definition des méthodes
+
+template <class NType>
+CMatrix<NType>::CMatrix(unsigned int uiRow, unsigned int uiCol, NType **ppTab)
+{
+	uiMATRow = uiRow;
+	uiMATCol = uiCol;
+	ppMATMatrix = ppTab;
+}
+
+template <class NType>
+ CMatrix<NType>::CMatrix(const CMatrix & MATParam)
+{
+
+}
+
+ template <class NType>
+ CMatrix<NType>::CMatrix(unsigned int uiRow, unsigned int uiCol)
+{
+	 uiMATRow = uiRow;
+	 uiMATCol = uiCol;
+	 NType ppTab[uiRow][uiCol];
+	 for (unsigned int uiLoop = 0; uiLoop < uiRow; uiLoop++)
+	 {
+		 for (unsigned int uiLoop2 = 0; uiLoop2 < uiCol; uiLoop2++)
+		 {
+			 ppTab[uiLoop][uiLoop2] = 0;
+		 }
+	 }
+	 ppMATMatrix = ppTab;
+
+}
+
+ template <class NType>
+CMatrix<NType>& CMatrix<NType>::operator*(double c)
+{
+	NType **ppTab = ppMATMatrix;
+	for (unsigned int uiLoop = 0; uiLoop < uiMATRow; uiLoop++)
+	{
+		for (unsigned int uiLoop2 = 0; uiLoop2 < uiMATCol; uiLoop2++)
+		{
+			ppTab[uiLoop][uiLoop2] *= c;
+		}
+	}
+	return new CMatrix(uiMATRow,uiMATCol,ppTab);
+}
+
+template <class NType>
+CMatrix<NType> operator*(double c, CMatrix<NType> MATParam)
+{
+	NType **ppTab = MATParam.ppMATMatrix;
+	for (unsigned int uiLoop = 0; uiLoop < MATParam.uiMATRow; uiLoop++)
+	{
+		for (unsigned int uiLoop2 = 0; uiLoop2 < MATParam.uiMATCol; uiLoop2++)
+		{
+			ppTab[uiLoop][uiLoop2] *= c;
+		}
+	}
+
+	return new CMatrix<NType>(MATParam.uiMATRow,MATParam.uiMATCol,ppTab);
+}
+
+template <class NType>
+CMatrix<NType> CMatrix<NType>::operator/(double c)
+{
+    if(c == 0)
+    {
+        throw new CException(DIV_PAR_0);;
+    }
+
+	NType **ppTab = ppMATMatrix;
+	for (unsigned int uiLoop = 0; uiLoop < uiMATRow; uiLoop++)
+	{
+		for (unsigned int uiLoop2 = 0; uiLoop2 < uiMATCol; uiLoop2++)
+		{
+			ppTab[uiLoop][uiLoop2] /= c;
+		}
+	}
+	return new CMatrix(uiMATRow,uiMATCol,ppTab);
+}
+
+template <class NType>
+CMatrix<NType> CMatrix<NType>::operator+(CMatrix MATParam)
+{
+    if(uiMATCol != MATParam.uiMATCol || uiMATRow != MATParam.uiMATRow)
+        throw new CException(BAD_SIZE_OF_MAT);
+
+    NType **ppTab = ppMATMatrix;
+	for (unsigned int uiLoop = 0; uiLoop < uiMATRow; uiLoop++)
+	{
+		for (unsigned int uiLoop2 = 0; uiLoop2 < uiMATCol; uiLoop2++)
+		{
+			ppTab[uiLoop][uiLoop2] += MATParam.ppMATMatrix[uiLoop][uiLoop2];
+		}
+	}
+	return new CMatrix(uiMATRow,uiMATCol,ppTab);
+
+}
+
+
+
+
+/** \brief
+ *Peut-être problème si NType = unsigned int et que l'on soustrait le + petit par le plus grand
+ * \param
+ * \param
+ * \return
+ *
+ */
+template <class NType>
+CMatrix<NType> CMatrix<NType>::operator-(CMatrix MATParam)
+{
+	 if(uiMATCol != MATParam.uiMATCol || uiMATRow != MATParam.uiMATRow)
+        throw new CException(BAD_SIZE_OF_MAT);
+
+    NType **ppTab = ppMATMatrix;
+	for (unsigned int uiLoop = 0; uiLoop < uiMATRow; uiLoop++)
+	{
+		for (unsigned int uiLoop2 = 0; uiLoop2 < uiMATCol; uiLoop2++)
+		{
+			ppTab[uiLoop][uiLoop2] -= MATParam.ppMATMatrix[uiLoop][uiLoop2];
+		}
+	}
+	return new CMatrix(uiMATRow,uiMATCol,ppTab);
+}
+
+template <class NType>
+CMatrix<NType> CMatrix<NType>::operator*(CMatrix MATParam)
+{
+	return 0;
+}
+
+template <class NType>
+void CMatrix<NType>::MATPrint()
+{
+}
+
+template <class NType>
+CMatrix<NType> CMatrix<NType>::MATTranspose()
+{
+	return 0;
+}
+
+
+
+
+
+
+
+
 #endif
